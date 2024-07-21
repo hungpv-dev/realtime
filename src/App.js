e28dce85848fc17d2e2425bea6e096d45ba43fe4
hungpv <phamvanhung.dev@gmail.com>
2024-07-21 16:04:02 +0700

    DONE

--- src/App.js
+++ src/App.js
@@ -1,25 +1,44 @@
-import logo from './logo.svg';
-import './App.css';
+import { useEffect } from 'react';
+import Echo from 'laravel-echo';
+import { io } from 'socket.io-client';
 
-function App() {
+const App = () => {
+  useEffect(() => {
+    const appId = 'cb87f9d5d3527165';
+    const key = '2990bec3f11d88e7af6136259fb00082';
+
+    window.Echo = new Echo({
+      broadcaster: "socket.io",
+      client: io,
+      host: "http://localhost:6001",
+      authHost: 'http://localhost:8000',
+      key: key,
+      appId: appId,
+      authEndpoint: '/broadcasting/auth',
+    });
+
+    console.log(window.Echo);
+
+    window.Echo.connector.socket.on("connect", function () {
+      console.log("Kết nối thành công");
+    });
+
+    window.Echo.connector.socket.on("disconnect", function () {
+      console.log("Đã ngắt kết nối");
+    });
+
+    window.Echo.channel("is-online").listen("UserOnline", (event) => {
+      console.log(event);
+    });
+    console.log('hungpv');
+    return () => {};
+  }, []); 
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div>
+      <h1>Đây là ứng dụng realtime của tôi</h1>
     </div>
   );
-}
+};
 
 export default App;
